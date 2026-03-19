5462cc21f86f19d0ac5d3b0ac929c2089296764f
Gregoryck <gregorycastillothen@gmail.com>
2026-03-19 23:14:40 +0000

    1

---
+++ Midterm/Part 3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+    int x 
+    cout<<"enter a two-digit number: ";
+    cin >>x;
+    int most=x/10
+    int least=x%10;
+
+    If(least !=0&& x % least == 0){
+        cout <<least<"is a factor of"<<x<<endl;
+    }
+    int root = sqrt(x);
+    if(root*root==x){
+        cout<<x<<"is a perfect square"<<endl;
+    }
+    return 0;
+}
